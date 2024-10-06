0005c0454f65ebfc3760c781c171477c896513cb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-06 21:19:26 +0530

    updated

--- src/Cyl.jsx
+++ src/Cyl.jsx
@@ -1,17 +1,22 @@
 
 import { useTexture } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 import * as THREE from 'three';
 
 
     
 const Cyl = () => {
     let tex = useTexture("./img4.png");
-    
+    let cyl = useRef(null);
+    useFrame((state, delta) => {
+        cyl.current.rotation.y += delta;
+    })
 
     return (
-        <mesh>
-            <cylinderGeometry args={[1, 1, 1, 30, 30, true]} />
-            <meshStandardMaterial map={tex} side={THREE.DoubleSide} />
+        <mesh ref={cyl} rotation={[0, 0.2, 0.2]}>
+            <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+            <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
         </mesh>
     )
 }
