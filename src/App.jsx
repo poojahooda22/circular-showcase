c971edd2b6bbf076865564af66a3d2a550df01f5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-07 08:41:32 +0530

    updated bloom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Canvas } from '@react-three/fiber'
 import './App.css'
 import { OrbitControls } from '@react-three/drei';
 import Cyl from './Cyl';
-import { Bloom } from '@react-three/postprocessing';
+import { Bloom, EffectComposer } from '@react-three/postprocessing';
 
 function App() {
   return (
@@ -10,12 +10,14 @@ function App() {
       <OrbitControls />
       <ambientLight />
       <Cyl/>
+      <EffectComposer>
       <Bloom
         intensity={1.0} // The bloom intensity.
         luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
         luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
         mipmapBlur
       />
+      </EffectComposer>
     </Canvas>
   )
 }
