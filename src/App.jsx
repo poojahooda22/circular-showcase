0760f7ec2b51a8101b52e4932f3cc030a7172e86
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-07 10:03:09 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,15 @@ import { Bloom, EffectComposer } from '@react-three/postprocessing';
 
 function App() {
   return (
-    <Canvas>
+    <Canvas flat camera={{fov: 30}}>
       <OrbitControls />
       <ambientLight />
       <Cyl/>
       <EffectComposer>
         <Bloom
-          intensity={1.0} // The bloom intensity.
+          intensity={0.0} // The bloom intensity.
           luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
           luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
-          mipmapBlur
         />
       </EffectComposer>
     </Canvas>
