07d5ae25bcfb86a4277c9a4392c8bbb9f92f959b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-07 08:42:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Bloom
           intensity={1.0} // The bloom intensity.
           luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
-        luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+          luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
         mipmapBlur
         />
       </EffectComposer>
