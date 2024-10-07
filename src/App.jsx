84a3accbfd6f67f30011289f152f48f50b1bd918
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-07 08:41:46 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <ambientLight />
       <Cyl/>
       <EffectComposer>
-      <Bloom
+        <Bloom
         intensity={1.0} // The bloom intensity.
         luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
         luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
