55be11efdfe2b43b60cb78329f68448391ab241e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-07 08:41:07 +0530

    removed false

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         intensity={1.0} // The bloom intensity.
         luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
         luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
-        // mipmapBlur={false} 
+        mipmapBlur
       />
     </Canvas>
   )
